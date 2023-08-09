416fcc4230203ae346bd71b6e46ad8de9eabceaf
dohyeong kim <dohyeongkim001024@gmail.com>
2023-08-09 07:33:25 -0600

    navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+  ],
   theme: {
-    extend: {},
+    fontFamily: {
+      'montserrat': ['Montserrat', 'sans-serif']
+    },
+    extend: {
+      colors:{
+      },
+      brightness:{
+        25: '.25'
+      },
+      backgroundImage: {
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+  ],
 }
-
